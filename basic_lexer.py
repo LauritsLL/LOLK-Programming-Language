27c5f49ae65a8e20be8aae50eba3cad7f89b6a5a
Lause132 <lll2005@live.dk>
2020-02-23 09:05:37 +0100

    Added comments and docstrings to code.

--- basic_lexer.py
+++ basic_lexer.py
@@ -4,11 +4,14 @@ from sly import Lexer
 class BasicLexer(Lexer):
     """BASIC Lexer class for generating tokens."""
     tokens = {NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUN, TO, ARROW, EQEQ}
+
+    # Ignore spaces and tabs.
     ignore = "\t "
 
+    # One-character literals.
     literals = {'=', '+', '-', '*', '/', '(', ')', ',', ';'}
 
-    # Define the tokens.
+    # Define tokens (regular expressions REGEXES).
     IF = r'IF'
     THEN = r'THEN'
     ELSE = r'ELSE'
@@ -23,27 +26,43 @@ class BasicLexer(Lexer):
 
     @_(r'\d+')
     def NUMBER(self, t):
+        """When we get a number."""
         t.value = int(t.value)
         return t
 
     @_(r'#.*')
     def COMMENT(self, t):
+        """Commenting in BASIC should just be ignored."""
         pass
 
     @_(r'\n+')
     def newline(self, t):
+        """Count newlines for better error handling."""
         self.lineno = t.value.count('\n')
+
+    def error(self, t):
+        """Handling errors in the code."""
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
 
 
 if __name__ == '__main__':
     lexer = BasicLexer()
+
+    # Keep track of variables when interpreter is running.
     env = {}
+
+    # Infinite loop that accepts code.
     while True:
         try:
-            text = input('basic > ')
+            data = input('BASIC> ')
         except EOFError:
             break
-        if text:
+
+        # If we got the code successfully - generate tokens for parser.
+        if data:
             lex = lexer.tokenize(text)
+
+            # For now, just print the tokens (We haven't implemented parser yet!)
             for token in lex:
                 print(token)
