7acde1e2ec791916b7b998a46246bb56232b32e6
Lause132 <lll2005@live.dk>
2020-02-25 16:52:36 +0100

    Added README to project and refactored compiler and interpreter in to seperate files.

---
+++ README.md
@@ -0,0 +1,29 @@
+# LOLK
+
+## Installation
+This project depends on the `sly` library.
+Install it with: `$ pip3 install sly`
+
+Then you need to,
+1. Clone the repository: `git clone https://github.com/Lause132/LOLK-Programming-Language.git`
+2. Run the `lang_interpreter.py` or the `lang_compiler.py` file, which depends on if you want to use the in-built interpreter or [compile](#use_compiler) a file.
+
+## Use the in-built interpreter.
+Run the file `lang_interpreter.py` and it will automatically enter the prompt, where you can type code.
+
+## <a name="use_compiler"></a>Use the compiler
+To compile your own files, run the `lang_compiler.py` file, and type in the path to your file. F.ex.:
+
+`What file do you want to use (Starting in root directory {DIRECTORY}) test_lang.lolk`
+
+This would run the interpreter on the `test_lang.lolk` file and display the output in real-time.
+
+## Authors
+
+* **Laurits L. Lolck** - *Main developer of project*
+
+## Use of this code
+
+This code can be used freely commercially or personal.
+
+> But I would very much appreciate if you would acknowledge me in your code and link to this project.
--- lang_interpreter.py
+++ lang_interpreter.py
@@ -5,19 +5,8 @@ from lang_parser import LangParser
 from lang_execute import Execute
 
 
-# Use interpreter or run compiler on file?
-use_interpreter = False
-run_file_compiler = False
-
-inter_or_comp = input("Use 'interpreter' or run 'compiler' on file? ")
-if inter_or_comp.lower() == "interpreter":
-    use_interpreter = True
-elif inter_or_comp.lower() == "compiler":
-    run_file_compiler = True
-
-
-# Only if the user wants to use the interpreter.
-if __name__ == '__main__' and use_interpreter:
+# Start interpreter.
+if __name__ == '__main__':
     lexer = LangLexer()
     parser = LangParser()
 
@@ -38,45 +27,3 @@ if __name__ == '__main__' and use_interpreter:
 
             # Run parsed tree and generate output.
             Execute(tree, env)
-
-
-# Run compiler on a file.
-if run_file_compiler:
-    lexer = LangLexer()
-    parser = LangParser()
-
-    # Keep track of variables when compiler is running.
-    env = {}
-
-    while(True):
-        prompt = "What file do you want to use (Starting in root directory "
-        prompt += os.getcwd() + ") "
-        file_to_use = input(prompt)
-        program = os.path.join(os.getcwd(), file_to_use)
-        if os.path.exists(program) and os.path.isfile(program):
-            # The file exists; compile and run.
-            print("Running specified file with compiler...\n\n")
-            print("\tSTART OF PROGRAM")
-            print("----------------------------------------\n\n")
-            
-            # Split code up into seperate lines and add End Of File to end of list,
-            # to know when we are done executing the file.
-            code_lines = open(program, 'r').read().split("\n")
-            code_lines.append("<EOF>")
-
-            # Go through every line of code and execute it.
-            for code in code_lines:
-                # If we get <EOF> break loop.
-                if code == "<EOF>":
-                    break
-                tree = parser.parse(lexer.tokenize(code))
-
-                # Run parsed tree and generate output.
-                Execute(tree, env)
-            
-            
-            # Break out of loop after execution.
-            break
-        else:
-            # File doesn't exist - Inform user.
-            print("File specified in directory " + program + " does not exist!")
